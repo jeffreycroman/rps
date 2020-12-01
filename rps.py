aed8b0dcc0fdc233d91fca205bc93bbc9a5c7f18
jeffreycroman <40694827+jeffreycroman@users.noreply.github.com>
2020-11-30 22:33:28 -0500

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,18 +1,31 @@
 from random import *
 items = ["ROCK", "PAPER", "SCISSORS"]
 print("Welcome to the rock, paper, scissors thunderdome!")
-player = ''
-while player not in items:
-    player = input("Choose your rock, paper or scissors.").upper()
-print("You chose,", player)
-computer = choice(items)
-print("I chose,",computer)
+
 
 rules = {"ROCK":"SCISSORS","PAPER":"ROCK","SCISSORS":"PAPER"}
 
-if player == computer:
-    print("We drew")
-elif rules[player] == computer:
-    print("You won")
-else:
-    print("I won!")
+scorecpu = 0
+scoreplayer = 0
+
+while scoreplayer < 5 and scorecpu < 5:
+    player = ''
+    while player not in items:
+        player = input("Choose your rock, paper or scissors.").upper()
+    print("You chose,", player)
+    computer = choice(items)
+    print("I chose,",computer)
+    if player == computer:
+        print("We drew")
+    elif rules[player] == computer:
+        scoreplayer = scoreplayer + 1
+        print("You won this round, the score is:",scoreplayer,"for you and",scorecpu,"for me.")
+    else:
+        scorecpu = scorecpu + 1
+        print("I won this round, the score is:",scoreplayer,"for you and",scorecpu,"for me.")
+
+if scoreplayer == 5:
+    print("fine, you beat me this time.... play again???")
+
+if scorecpu == 5:
+    print("boom zamaray! I beat you - I bet I can do it again")
